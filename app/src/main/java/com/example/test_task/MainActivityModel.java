258746270ab66b9137f397f8f5f5202c0884b806
Mikhail Plekhanov <vbifgktfyjd@mail.ru>
2019-01-26 02:07:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test_task/MainActivityModel.java
@@ -0,0 +1,14 @@
+package com.example.test_task;
+
+import android.provider.ContactsContract;
+
+import org.json.JSONObject;
+
+public class MainActivityModel {
+
+    String url = "https://gateway.watsonplatform.net/language-translator/api";
+    String username = "6987a48d-342e-4a69-8adc-e65b1cc0b9da";
+    String password = "MxYSIi6nQP2Y";
+
+    //ibmcloud cf cups Personality-Insights-Std -p "\"6987a48d-342e-4a69-8adc-e65b1cc0b9da\":\"MxYSIi6nQP2Y\""
+}
---
+++ app/src/main/java/com/example/test_task/MainActivityViewModel.java
@@ -0,0 +1,27 @@
+package com.example.test_task;
+
+import android.content.Context;
+
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivityViewModel {
+
+    void showFragmentNewText (FragmentManager fragmentManager){
+
+        FragmentNewText fragmentNewText = new FragmentNewText();
+        fragmentManager.beginTransaction().replace(R.id.fragment, fragmentNewText).commit();
+    }
+
+    void showFragmentHistory (FragmentManager fragmentManager){
+
+        FragmentHistory fragmentHistory = new FragmentHistory();
+        fragmentManager.beginTransaction().replace(R.id.fragment, fragmentHistory).commit();
+
+    }
+
+    void identifyLanguage (String s){
+
+        MainActivityModel mainActivityModel = new MainActivityModel();
+
+    }
+}
